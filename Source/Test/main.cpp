37a44a6ff24fcfc8e7843bb4b527a4b05fb75711
MrDazLive <daz.livermore@hotmail.com>
2017-11-16 22:42:30 +0000

    ALL_TESTS:  - Changed name format for test projects from "*-Test" to "Test-*".  - Modified ALL_TESTS main for platform specific. Not too happy with the outcome, but it'll do for now. D3DE Environment:  - Reverted travis to premulti-os attempts.

--- Source/Test/main.cpp
+++ Source/Test/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <list>
 
+#ifdef _WIN32
+#ifdef _DEBUG
+std::string run = std::string("cd /d \"") + OUTPUT_DIRECTORY + "/Debug\" && Test-";
+#else
+std::string run = std::string("cd /d \"") + OUTPUT_DIRECTORY + "/Release\" && Test-";
+#endif
+#else
+std::string run = "./Test-";
+#endif
+
 std::list<const char*> tests {
-  "./Types-Test",
-  "./Core-Test"
+  "Types",
+  "Core"
 };
 
 int main(int argc, char **args) {
   for(auto test : tests) {
-    if(system(test))
+    if(system((run + test).c_str()))
       std::cin.ignore();
   }
 }
